11ca57557ba13a60adf7b3f89c0f091bd447b90f
quang nguyen tan <tanquangyds@gmail.com>
2021-01-13 21:18:27 +0700

    udemy

--- myfirstreact/src/App.js
+++ myfirstreact/src/App.js
@@ -1,27 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
-import styled from 'styled-components';
 
 import Person from './Person/Person'
 // import UserInput from './UserInput/UserInput';
 // import UserOutput from './UserOutput/UserOutput';
 
 function App(props) {
-  const StyledButton = styled.button`
-    background-color: ${props => props.alt ? 'blue' : 'green'};
-    padding: 16px;
-    margin: 16px;
-    fontSize: 16px;
-    fontWeight: bold;
-    color: white;
-    border: none;
-    outline: none;
-
-    :hover {
-      background-color: ${props => props.alt ? 'lightblue' : 'lightgreen'};
-      color: black
-    }
-  `;
+  
   const [persons, setPersons] = useState(
     [
       {id: 0, name: 'Quang', age: '28'},
@@ -84,9 +69,9 @@ function App(props) {
   return (
 
     <div className="App">
-      <StyledButton onClick={toggleHandler}
+      <button style= {style} onClick={toggleHandler}
       alt={showPerson}
-      >Toggle</StyledButton>
+      >Toggle</button>
       {person}
     </div>
   )
